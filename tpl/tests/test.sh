0ca8620b521a49340306dd3032856b31c3b09c22
ava <ava1280@yandex.ru>
2019-09-07 14:24:55 +0300

    * test.sh: + выбор нескольких тестов

--- tpl/tests/test.sh
+++ tpl/tests/test.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-if [[ -n $1 ]]
-then
-  if [[ -a "cases/$1.tpl" ]]; then
-    php ../../tpl.php "$1"
-  else
-    echo Test not found
-  fi
-else
-  php ../../tpl.php
-fi
+php ../../tpl.php $*
 echo
